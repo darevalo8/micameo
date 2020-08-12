92ac096cef90638967498d919394bf79ca4c266a
Daniel Arevalo <acuentas9508@hotmail.com>
2020-08-12 17:41:45 -0500

    estos cambios son de la gestion de usuario y categorias para poder editar y leer los datos

--- micameo/users/api/serializers/talent_serializer.py
+++ micameo/users/api/serializers/talent_serializer.py
@@ -8,28 +8,6 @@ class TalentSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     categories = serializers.StringRelatedField(many=True, read_only=True)
 
-    # def to_internal_value(self, data):
-    #     print(data["user"]['username'])
-    #     categories = []
-    #     for category in data['categories']:
-    #         print(category)
-    #         try:
-    #             category_object = SubCategory.objects.get(sub_name=category)
-    #             categories.append(category_object)
-    #         except SubCategory.DoesNotExist:
-    #             pass
-    #
-    #     res = {
-    #         "user": User.objects.get(username=data["user"]['username']),
-    #         "profile_image": data['profile_image'],
-    #         "description": data['description'],
-    #         "response_days": data['response_days'],
-    #         "price": data['price'],
-    #         "categories": categories,
-    #         "birthday": data['birthday']
-    #     }
-    #     return res
-
     class Meta:
         model = Talent
         fields = ['user', 'profile_image', 'description', 'response_days', "price", "url", "categories", "birthday"]
@@ -39,4 +17,3 @@ class TalentSerializer(serializers.ModelSerializer):
             "user": {"required": False},
 
         }
-        # depth = 1
--- micameo/users/models/client_model.py
+++ micameo/users/models/client_model.py
@@ -1,5 +1,6 @@
 from .user_model import BaseUser
 
+
 class Client(BaseUser):
     def __str__(self):
         return str(self.user)
